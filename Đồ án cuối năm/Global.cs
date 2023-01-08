8bb5febd8f0b744010230036ff1fbca8b0b1c686
Khoa Trần Đăng <trandgkhoa2602@gmail.com>
2023-01-09 01:02:58 +0700

    Thêm DataProvider và Global

---
+++ Đồ án cuối năm/Global.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Đồ_án_cuối_năm
+{
+    internal class Global
+    {
+        public static int current_ID = 0;
+        public static string current_user = "";
+        public static string current_username = "";
+        public static string current_pass = "";
+    }
+}
